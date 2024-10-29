105d0984c1b0958efbf2d090a145f5204f22dc1a
Simone Gianni <simoneg@apache.org>
2024-10-29 03:59:27 +0100

    renamed

--- components/metriful/__init__.py
+++ components/metriful/__init__.py
@@ -17,7 +17,6 @@ CONFIG_SCHEMA = (
 )
 
 async def to_code(config):
-    logging.info("I'm here")
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
