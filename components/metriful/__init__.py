f50be2453fd92c831ef959e5f9475c61b2b83c66
Simone Gianni <simoneg@apache.org>
2024-10-31 17:30:09 +0100

    debug sensor not showing up

--- components/metriful/__init__.py
+++ components/metriful/__init__.py
@@ -2,8 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
 from esphome.const import (
-    UNIT_CELSIUS, UNIT_PERCENT, UNIT_OHM, UNIT_PARTS_PER_MILLION, UNIT_LUX, UNIT_PA, UNIT_DBA, 
-    UNIT_DBC, UNIT_MICROGRAM_PER_CUBIC_METER, DEVICE_CLASS_TEMPERATURE, 
+    UNIT_CELSIUS, UNIT_PERCENT, UNIT_OHM, UNIT_PARTS_PER_MILLION, UNIT_LUX, UNIT_PASCAL, 
+    UNIT_DB, UNIT_MICROGRAM_PER_CUBIC_METER, DEVICE_CLASS_TEMPERATURE, 
     DEVICE_CLASS_HUMIDITY, DEVICE_CLASS_PRESSURE, DEVICE_CLASS_SOUND_PRESSURE,
     DEVICE_CLASS_ILLUMINANCE, DEVICE_CLASS_AQI, DEVICE_CLASS_PM25, 
     STATE_CLASS_MEASUREMENT, CONF_ID
@@ -39,7 +39,7 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MetrifulComponent),
         cv.Optional(CONF_TEMPERATURE, default=SENSOR_SCHEMA.unit_of_measurement(UNIT_CELSIUS).accuracy_decimals(1).device_class(DEVICE_CLASS_TEMPERATURE).state_class(STATE_CLASS_MEASUREMENT)): sensor.sensor_schema(),
-        cv.Optional(CONF_PRESSURE, default=SENSOR_SCHEMA.unit_of_measurement(UNIT_PA).accuracy_decimals(0).device_class(DEVICE_CLASS_PRESSURE)): sensor.sensor_schema(),
+        cv.Optional(CONF_PRESSURE, default=SENSOR_SCHEMA.unit_of_measurement(UNIT_PASCAL).accuracy_decimals(0).device_class(DEVICE_CLASS_PRESSURE)): sensor.sensor_schema(),
         cv.Optional(CONF_HUMIDITY, default=SENSOR_SCHEMA.unit_of_measurement(UNIT_PERCENT).accuracy_decimals(1).device_class(DEVICE_CLASS_HUMIDITY)): sensor.sensor_schema(),
         cv.Optional(CONF_PARTICLE_DUTY, default=SENSOR_SCHEMA.unit_of_measurement(UNIT_PERCENT).accuracy_decimals(2).device_class(DEVICE_CLASS_PM25)): sensor.sensor_schema(),
         cv.Optional(CONF_PARTICLE_CONC, default=SENSOR_SCHEMA.unit_of_measurement(UNIT_MICROGRAM_PER_CUBIC_METER).accuracy_decimals(2).device_class(DEVICE_CLASS_PM25)): sensor.sensor_schema(),
@@ -50,9 +50,9 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Optional(CONF_AQI_ACC, default=SENSOR_SCHEMA.accuracy_decimals(0).device_class(DEVICE_CLASS_AQI)): sensor.sensor_schema(),
         cv.Optional(CONF_ILLUMINANCE, default=SENSOR_SCHEMA.unit_of_measurement(UNIT_LUX).accuracy_decimals(2).device_class(DEVICE_CLASS_ILLUMINANCE)): sensor.sensor_schema(),
         cv.Optional(CONF_W_LIGHT, default=SENSOR_SCHEMA.accuracy_decimals(0).device_class(DEVICE_CLASS_ILLUMINANCE)): sensor.sensor_schema(),
-        cv.Optional(CONF_SOUND_SPL, default=SENSOR_SCHEMA.unit_of_measurement(UNIT_DBA).accuracy_decimals(1).device_class(DEVICE_CLASS_SOUND_PRESSURE)): sensor.sensor_schema(),
+        cv.Optional(CONF_SOUND_SPL, default=SENSOR_SCHEMA.unit_of_measurement("dBa").accuracy_decimals(1).device_class(DEVICE_CLASS_SOUND_PRESSURE)): sensor.sensor_schema(),
         cv.Optional(CONF_SOUND_PEAK, default=SENSOR_SCHEMA.unit_of_measurement("mPa").accuracy_decimals(2).device_class(DEVICE_CLASS_SOUND_PRESSURE)): sensor.sensor_schema(),
-        cv.Optional(CONF_SOUND_BANDS, default=[SENSOR_SCHEMA.unit_of_measurement(UNIT_DBC).accuracy_decimals(1).device_class(DEVICE_CLASS_SOUND_PRESSURE)] * 6): cv.All([sensor.sensor_schema()], cv.ensure_list_length(6)),
+        cv.Optional(CONF_SOUND_BANDS, default=[SENSOR_SCHEMA.unit_of_measurement(UNIT_DB).accuracy_decimals(1).device_class(DEVICE_CLASS_SOUND_PRESSURE)] * 6): cv.All([sensor.sensor_schema()], cv.ensure_list_length(6)),
     }
 ).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x01))
 
