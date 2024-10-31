4f77f16fa5e8154c18610bcd7cd873f6f2a65490
Simone Gianni <simoneg@apache.org>
2024-10-31 20:29:10 +0100

    debug sensor not showing up

--- components/metriful/__init__.py
+++ components/metriful/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.somponents.sensor import StateClasses
+from esphome.components.sensor import StateClasses
 from esphome.core import (ID)
 from esphome.const import (
     UNIT_CELSIUS, UNIT_PERCENT, UNIT_OHM, UNIT_PARTS_PER_MILLION, UNIT_LUX, UNIT_PASCAL, 
