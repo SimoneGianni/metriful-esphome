0b9401f1de88af4e7b0a5dce9cfa6ca6ff330da6
Simone Gianni <simoneg@apache.org>
2024-10-31 19:49:34 +0100

    debug sensor not showing up

--- components/metriful/__init__.py
+++ components/metriful/__init__.py
@@ -78,7 +78,7 @@ async def to_code(config):
             sens = await sensor.new_sensor(config[key])
         else:
             sens = await sensor.new_sensor({
-                "id": key,
+                "id": cv.declare_id(sensor.Sensor),
                 "name": name,
                 "unit_of_measurement": unit,
                 "accuracy_decimals": accuracy_decimals,
