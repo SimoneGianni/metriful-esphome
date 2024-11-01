ab5bd67cfe8e1005c1be0902f554003822f12e8c
Simone Gianni <simoneg@apache.org>
2024-11-01 14:03:27 +0100

    i2c communications set cycle

--- components/metriful/MS430_ESPHome.h
+++ components/metriful/MS430_ESPHome.h
@@ -191,6 +191,7 @@ class MS430 :  public i2c::I2CDevice, public Component
     }
 
     void loop() override {
+      ESP_LOGV(TAG, "In the loop");
       if (ready_assertion_event)
       {
         ESP_LOGV(TAG, "Got assertion event");
